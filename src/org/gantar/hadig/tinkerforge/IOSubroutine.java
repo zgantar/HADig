3efddd11a21b300743b38870da5e3562de15448d
Ziga <zgantar@yahoo.com>
2020-11-28 11:01:02 +0100

    Removed unused code

--- src/org/gantar/hadig/tinkerforge/IOSubroutine.java
+++ src/org/gantar/hadig/tinkerforge/IOSubroutine.java
@@ -16,80 +16,80 @@ public class IOSubroutine implements DiagnosticInterface {
 
     @Override
     public void diagnose(HashMap<String, HashMap<String, Device>> deviceTree, ResourceBundle resources, IPConnection ipcon) {
-        HashMap<String, Device> devices = deviceTree.get(BrickletIO4.DEVICE_DISPLAY_NAME);
-        String[] io4s = new String[0];
-
+//        HashMap<String, Device> devices = deviceTree.get(BrickletIO4.DEVICE_DISPLAY_NAME);
+//        String[] io4s = new String[0];
+//
+//        try {
+//            io4s = resources.getString("IO4").split("_");
+//        } catch (MissingResourceException e) {
+//            System.out.println("Napaka pri iskanju nastavitev: " + BrickletIO4.DEVICE_DISPLAY_NAME + " ali " + BrickletIO16.DEVICE_DISPLAY_NAME + "!");
+//            e.printStackTrace();
+//        }
+//
+//        HashMap<String, Boolean> deviceVisits = new HashMap<>();
+//        for (String uid : io4s) {
+//            deviceVisits.put(uid, false);
+//        }
+//
         try {
-            io4s = resources.getString("IO4").split("_");
-        } catch (MissingResourceException e) {
-            System.out.println("Napaka pri iskanju nastavitev: " + BrickletIO4.DEVICE_DISPLAY_NAME + " ali " + BrickletIO16.DEVICE_DISPLAY_NAME + "!");
-            e.printStackTrace();
-        }
-
-        HashMap<String, Boolean> deviceVisits = new HashMap<>();
-        for (String uid : io4s) {
-            deviceVisits.put(uid, false);
-        }
-
-        try {
-            if (devices != null && devices.size() > 0) {
-//                System.out.println("Preverjam število IO4, ki so odgovoril");
-                int i = io4s.length;
-                for (Device device : devices.values()) {
-                    for (String io4UID : io4s) {
-                        if (device.getIdentity().uid.equals(io4UID)) {
-//                            System.out.println("Našel IO4 " + device.getIdentity().uid);
-                            deviceVisits.put(io4UID, true);
-                            i--;
-                            break;
-                        }
-                    }
-                }
-
-                if (i != 0) {
-                    System.out.println("!!!!!!Število zapisov v nastavitvah ne ustreza številu IO4!!!!");
-                    for (Map.Entry entry : deviceVisits.entrySet()) {
-                        if (!(boolean) entry.getValue()) {
-                            if (!Util.resetTinkerforge(resources, "/")) {
-                                return;
-                            }
-                        }
-                    }
-                    //tukaj potrebno dodat reset elektrike!!!!!
-                    return;
-                }
-
-
-                if (devices.size() > 0) {
-                    for (Map.Entry<String, Device> entry : devices.entrySet()) {
-                        BrickletIO4 device = (BrickletIO4) entry.getValue();
-                        Device.Identity identity = device.getIdentity();
-
-                        BrickletIO4.Configuration conf = device.getConfiguration();
-                        if (conf.directionMask != Integer.parseInt(resources.getString("IO4_" + identity.uid))) {
-                            System.out.println("!!!!!!Trenutne nastavitve niso pravilne za IO4 " + identity.uid + "!!!!");
-                            //first the input side
-                            String settingsInput = resources.getString("Conf_" + identity.uid + "_i");
-                            if (!settingsInput.equals("")) {
-                                String[] elements = settingsInput.split("_");
-                                System.out.println("!!!!!!Nastavljam input");
-                                device.setConfiguration(Short.parseShort(elements[0]), elements[1].toCharArray()[0], Boolean.parseBoolean(elements[2]));
-                            }
-                            //then the output side
-                            String settingsOutput = resources.getString("Conf_" + identity.uid + "_o");
-                            if (!settingsOutput.equals("")) {
-                                String[] elements = settingsOutput.split("_");
-                                System.out.println("!!!!!!Nastavljam output");
-                                device.setConfiguration(Short.parseShort(elements[0]), elements[1].toCharArray()[0], Boolean.parseBoolean(elements[2]));
-                            }
-//                    device.setValue((short) Integer.parseInt(resources.getString("IO4_" + identity.uid)));
-//                    Util.reset(ipcon, identity);
-                        }
-                    }
-                }
-            }
-
-            devices = deviceTree.get(BrickletIO16.DEVICE_DISPLAY_NAME);
+//            if (devices != null && devices.size() > 0) {
+////                System.out.println("Preverjam število IO4, ki so odgovoril");
+//                int i = io4s.length;
+//                for (Device device : devices.values()) {
+//                    for (String io4UID : io4s) {
+//                        if (device.getIdentity().uid.equals(io4UID)) {
+////                            System.out.println("Našel IO4 " + device.getIdentity().uid);
+//                            deviceVisits.put(io4UID, true);
+//                            i--;
+//                            break;
+//                        }
+//                    }
+//                }
+//
+//                if (i != 0) {
+//                    System.out.println("!!!!!!Število zapisov v nastavitvah ne ustreza številu IO4!!!!");
+//                    for (Map.Entry entry : deviceVisits.entrySet()) {
+//                        if (!(boolean) entry.getValue()) {
+//                            if (!Util.resetTinkerforge(resources, "/")) {
+//                                return;
+//                            }
+//                        }
+//                    }
+//                    //tukaj potrebno dodat reset elektrike!!!!!
+//                    return;
+//                }
+//
+//
+//                if (devices.size() > 0) {
+//                    for (Map.Entry<String, Device> entry : devices.entrySet()) {
+//                        BrickletIO4 device = (BrickletIO4) entry.getValue();
+//                        Device.Identity identity = device.getIdentity();
+//
+//                        BrickletIO4.Configuration conf = device.getConfiguration();
+//                        if (conf.directionMask != Integer.parseInt(resources.getString("IO4_" + identity.uid))) {
+//                            System.out.println("!!!!!!Trenutne nastavitve niso pravilne za IO4 " + identity.uid + "!!!!");
+//                            //first the input side
+//                            String settingsInput = resources.getString("Conf_" + identity.uid + "_i");
+//                            if (!settingsInput.equals("")) {
+//                                String[] elements = settingsInput.split("_");
+//                                System.out.println("!!!!!!Nastavljam input");
+//                                device.setConfiguration(Short.parseShort(elements[0]), elements[1].toCharArray()[0], Boolean.parseBoolean(elements[2]));
+//                            }
+//                            //then the output side
+//                            String settingsOutput = resources.getString("Conf_" + identity.uid + "_o");
+//                            if (!settingsOutput.equals("")) {
+//                                String[] elements = settingsOutput.split("_");
+//                                System.out.println("!!!!!!Nastavljam output");
+//                                device.setConfiguration(Short.parseShort(elements[0]), elements[1].toCharArray()[0], Boolean.parseBoolean(elements[2]));
+//                            }
+////                    device.setValue((short) Integer.parseInt(resources.getString("IO4_" + identity.uid)));
+////                    Util.reset(ipcon, identity);
+//                        }
+//                    }
+//                }
+//            }
+//
+            HashMap<String, Device> devices = deviceTree.get(BrickletIO16.DEVICE_DISPLAY_NAME);
             String[] io16s = new String[0];
 
             try {
@@ -99,7 +99,7 @@ public class IOSubroutine implements DiagnosticInterface {
                 e.printStackTrace();
             }
 
-            deviceVisits = new HashMap<>();
+            HashMap<String, Boolean> deviceVisits = new HashMap<>();
             for (String uid : io16s) {
                 deviceVisits.put(uid, false);
             }
